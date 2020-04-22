e7ca1b0ba37312b77e351c0f9485fb927ca82504
Paul Grochocinski <paul.grochocinski@gmail.com>
2020-04-22 14:58:06 -0400

    very basic movement logix

--- app/controllers/api/v1/battlesnake_games_controller.rb
+++ app/controllers/api/v1/battlesnake_games_controller.rb
@@ -18,7 +18,25 @@ module API
       end
 
       def move
-        possible_moves = ["up", "down", "left", "right"]
+        head = game_params[:you][:body].first
+        body = game_params[:you][:body][1..-1]
+
+        possible_moves = ["up", "right", "down", "left"]
+
+        # Simply check for borders and self if ok
+        if (head[:y] - 1 < 0) || (body.include?({x: head[:x], y: head[:y] - 1}))
+          possible_moves.delete("up")
+        end
+        if (head[:x] - 1 < 0) || (body.include?({x: head[:x] - 1, y: head[:y]}))
+          possible_moves.delete("left")
+        end
+        if (head[:y] + 1 > game_params[:board][:height]) || (body.include?({x: head[:x], y: head[:y] + 1}))
+          possible_moves.delete("down")
+        end
+        if (head[:x] + 1 > game_params[:board][:width]) || (body.include?({x: head[:x] + 1, y: head[:y]}))
+          possible_moves.delete("right")
+        end
+
         movement = { move: possible_moves.sample }
 
         render status: 200, json: movement
@@ -33,25 +51,17 @@ module API
       def game_params
         params.require(:battlesnake_game).permit(
           :turn,
-          game: [
-            :id
-          ],
+          game: [:id],
           board: [
             :height,
             :width,
-            food: [
-              :x,
-              :y
-            ],
+            food: [ :x, :y ],
             snakes: [
               :id,
               :name,
               :health,
               :shout,
-              body: [
-                :x,
-                :y
-              ]
+              body: [ :x, :y ]
             ]
           ],
           you: [
@@ -59,10 +69,7 @@ module API
             :name,
             :health,
             :shout,
-            body: [
-              :x,
-              :y
-            ]
+            body: [ :x, :y ]
           ]
         )
       end
