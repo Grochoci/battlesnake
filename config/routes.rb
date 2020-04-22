03cc64ed0613e279b14b1c21d9f723a7238fc0d4
Paul Grochocinski <paul.grochocinski@gmail.com>
2020-04-22 14:21:09 -0400

    adds shortcuts for game engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+
+
+  # Shortcuts to controller methods for game engine
+  post '/ping', to: 'api/v1/battlesnake_games#ping'
+  post '/start', to: 'api/v1/battlesnake_games#start'
+  post '/move', to: 'api/v1/battlesnake_games#move'
+  post '/end', to: 'api/v1/battlesnake_games#end'
+
   # Api namespace broken down into different versions for future management
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
@@ -12,5 +20,6 @@ Rails.application.routes.draw do
       end
     end
   end
+
 
 end
