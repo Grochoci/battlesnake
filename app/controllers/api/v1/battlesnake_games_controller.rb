0c32090a434624a5639d7a4df651040ca00ebb15
Paul Grochocinski <paul.grochocinski@gmail.com>
2020-04-22 13:55:33 -0400

    initial skeleton of required endpoint

--- app/controllers/api/v1/battlesnake_games_controller.rb
+++ app/controllers/api/v1/battlesnake_games_controller.rb
@@ -4,7 +4,67 @@ module API
       skip_before_action :verify_authenticity_token
 
       def ping
-        render status: 200, json: "pong"
+        render status: 200, json: {}
+      end
+
+      def start
+        appearance = {
+          color: "#888888",
+          head_type: "regular",
+          tail_type: "regular"
+        }
+
+        render status: 200, json: appearance
+      end
+
+      def move
+        possible_moves = ["up", "down", "left", "right"]
+        movement = { move: possible_moves.sample }
+
+        render status: 200, json: movement
+      end
+
+      def end
+        render status: 200, json: {}
+      end
+
+      private
+
+      def game_params
+        params.require(:battlesnake_game).permit(
+          :turn,
+          game: [
+            :id
+          ],
+          board: [
+            :height,
+            :width,
+            food: [
+              :x,
+              :y
+            ],
+            snakes: [
+              :id,
+              :name,
+              :health,
+              :shout,
+              body: [
+                :x,
+                :y
+              ]
+            ]
+          ],
+          you: [
+            :id,
+            :name,
+            :health,
+            :shout,
+            body: [
+              :x,
+              :y
+            ]
+          ]
+        )
       end
 
     end
